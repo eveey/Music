b824d884527e07a5bc1a45c11bbe88f6c17457cc
eveey <eva.shtos@gmail.com>
2018-10-01 22:53:27 +0200

    Create BaseViewModelTest

--- app/src/test/kotlin/com/evastos/music/ui/base/BaseViewModelTest.kt
+++ app/src/test/kotlin/com/evastos/music/ui/base/BaseViewModelTest.kt
@@ -1,22 +1,58 @@
 package com.evastos.music.ui.base
 
-import org.junit.After
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import com.evastos.music.RxImmediateSchedulerRule
+import com.jakewharton.rxrelay2.PublishRelay
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.verifyNoMoreInteractions
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class BaseViewModelTest {
 
+    @Rule
+    @JvmField
+    var testSchedulerRule = RxImmediateSchedulerRule()
+
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
+
+    private val networkConnectivityRelay = PublishRelay.create<Boolean>()
+
+    private val connectivityAction = mock<(Boolean) -> Unit>()
+
+    private lateinit var viewModel: TestViewModel
+
     @Before
     fun setUp() {
-    }
-
-    @After
-    fun tearDown() {
+        viewModel = TestViewModel()
     }
 
     @Test
-    fun onCreate() {
+    fun getNetworkConnectivityLiveData_withNetworkConnectivityEvents_invokesDistinctValues() {
+        viewModel.onCreate(networkConnectivityRelay, connectivityAction)
+
+        networkConnectivityRelay.accept(true)
+        networkConnectivityRelay.accept(true)
+        networkConnectivityRelay.accept(false)
+        networkConnectivityRelay.accept(true)
+        networkConnectivityRelay.accept(false)
+        networkConnectivityRelay.accept(false)
+
+        verify(connectivityAction, times(2)).invoke(true)
+        verify(connectivityAction, times(2)).invoke(false)
     }
+
+    @Test
+    fun getNetworkConnectivityLiveData_withNoNetworkConnectivityEvents_doesNotInvokeAnything() {
+        viewModel.onCreate(networkConnectivityRelay, connectivityAction)
+
+        verifyNoMoreInteractions(connectivityAction)
+    }
+
+    private inner class TestViewModel : BaseViewModel()
 }
